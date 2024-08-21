ffa708bb624508088ae64b57006f86cc390b8128
bakemoNie <em.verag@duocuc.cl>
2024-08-20 23:52:21 -0400

    Primer commit - Inicializando el proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'access',
+    loadChildren: () => import('./access/access.module').then( m => m.AccessPageModule)
+  },
 ];
 
 @NgModule({
